b11c690b7ffb412b48f2f27276ab6c5b529504d3
HalilCan <halilcanmemoglu@gmail.com>
2019-12-25 18:05:08 -0500

    Canvas works

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 let canvas, ctx;
+let screenW = 1200;
+let screenH = 900;
 
 function initCanvas() {
     canvas = document.getElementById('mainCanvas');
     ctx = canvas.getContext('2d');
 
     ctx.fillStyle = 'green';
-    ctx.fillRect(10, 10, 350, 100);
+    ctx.fillRect(0, 0, screenW, screenH);
 }
 
 window.onload = initCanvas;

b5fc7d5568fd4eba52f24dcf9534b6b38a9e6638
HalilCan <halilcanmemoglu@gmail.com>
2019-12-25 19:18:16 -0500

    raycasting works?

--- scripts/raytracer.js
+++ scripts/raytracer.js
@@ -1,13 +1,111 @@
-import {Vector, ImagePlane, Camera} from "./raytracer.js";
+//////////////////////////// Structs /////////////////////////////
+//export{Vector, ImagePlane, Camera, Ray};
 
+class Vector {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+  add(v) {
+    return new Vector(this.x + v.x, this.y + v.y, this.z + v.z);
+  }
+  subtract(v){
+    return new Vector(this.x - v.x, this.y - v.y, this.z - v.z);
+  }
+  scale(scalar) {
+    return new Vector(this.x * scalar, this.y * scalar, this.z * scalar);
+  }
+}
+
+class ImagePlane {
+  constructor(v1, v2, v3, v4) {
+    this.v1 = v1;
+    this.v2 = v2;
+    this.v3 = v3;
+    this.v4 = v4;
+  }
+}
+
+class Camera {
+  constructor(position) {
+    this.position = position;
+  }
+}
+
+class Ray{
+  constructor(origin, direction) {
+    this.origin = origin;
+    this.direction = direction;
+  }
+}
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////// Main Actions ////////////////////////
+//import {Vector, ImagePlane, Camera, Ray} from "./raytracerStructs";
+
+///////////// Setup /////////////
+let canvas, ctx;
+let screenW = 800;
+let screenH = 600;
 // Image Plane and Camera creation //
 let imPVec1 = new Vector(1, 0.75, 0);
 let imPVec2 = new Vector(-1, 0.75, 0);
 let imPVec3 = new Vector(1, -0.75, 0);
 let imPVec4 = new Vector(-1, -0.75, 0);
-let imagePlane = new ImagePlane(camVec1, camVec2, camVec3, camVec4);
+let imagePlane = new ImagePlane(imPVec1, imPVec2, imPVec3, imPVec4);
 
 let camPosition = new Vector(0, 0, -1);
 let camera = new Camera(camPosition);
-/////////////////////
-console.log(camera.x);
+/////////////////////////////////////
+/////////////////////////////////
+
+function initCanvas() {
+    canvas = document.getElementById('mainCanvas');
+    ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, 0, screenW, screenH);
+
+    castAllRays();
+}
+window.onload = initCanvas;
+rayCastDebug = 1;
+
+// Full ray casting //
+function castAllRays() {
+    for (let i = 0; i < screenW; i++) {
+        for (let j = 0; j < screenH; j++) {
+            let alpha = (i + 1) / screenW;
+            let beta = (j + 1) / screenH;
+            let p = bilinearInterpolate(alpha, beta);
+            let direction = p.subtract(camPosition);
+            let ray = new Ray(p, direction);
+
+            if (rayCastDebug) {
+                let red = alpha * 255;
+                let green = beta * 255;
+                let blue = 40;
+                ctx.fillStyle = "rgba("+red+","+green+","+blue+")";
+                ctx.fillRect(i, j, 1, 1);
+            }
+        }
+    }
+}
+////////////////////
+
+// bilinear interpolation for ray vector determination //
+function bilinearInterpolate(alpha, beta) {
+    let x1 = imPVec1, x2 = imPVec2, x3 = imPVec3, x4 = imPVec4;
+    
+    x3.scale(1-alpha);
+    x4.scale(alpha);
+
+    let t = (x1.scale(1-alpha)).add(x2.scale(alpha));
+    let b = (x3.scale(1-alpha)).add(x4.scale(alpha));
+    let p = t.scale(1-beta).add(b.scale(beta));
+    return p;
+}
+//////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////

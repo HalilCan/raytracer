a9b3864c1a6498c2317eeb81171fef16ca2b36f2
HalilCan <halilcanmemoglu@gmail.com>
2019-12-26 01:25:11 -0500

    I have no idea why expanding->flooring->contracting->expanding gives the correct values as opposed to the initial value. Is it the +1?

--- scripts/raytracer.js
+++ scripts/raytracer.js
@@ -1,3 +1,7 @@
+///////////////////////////// Flags //////////////////////////////
+let rayCastDebug = 1;
+//////////////////////////////////////////////////////////////////
+
 //////////////////////////// Structs /////////////////////////////
 //export{Vector, ImagePlane, Camera, Ray};
 
@@ -50,10 +54,10 @@ let canvas, ctx;
 let screenW = 800;
 let screenH = 600;
 // Image Plane and Camera creation //
-let imPVec1 = new Vector(1, 0.75, 0);
-let imPVec2 = new Vector(-1, 0.75, 0);
-let imPVec3 = new Vector(1, -0.75, 0);
-let imPVec4 = new Vector(-1, -0.75, 0);
+let imPVec1 = new Vector(-1, -0.75, 0);
+let imPVec2 = new Vector(1, -0.75, 0);
+let imPVec3 = new Vector(-1, 0.75, 0);
+let imPVec4 = new Vector(1, 0.75, 0);
 let imagePlane = new ImagePlane(imPVec1, imPVec2, imPVec3, imPVec4);
 
 let camPosition = new Vector(0, 0, -1);
@@ -63,7 +67,10 @@ let camera = new Camera(camPosition);
 
 function initCanvas() {
     canvas = document.getElementById('mainCanvas');
+    canvas.width = screenW;
+    canvas.height = screenH;
     ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, screenW, screenH);
 
     ctx.fillStyle = 'green';
     ctx.fillRect(0, 0, screenW, screenH);
@@ -71,23 +78,51 @@ function initCanvas() {
     castAllRays();
 }
 window.onload = initCanvas;
-rayCastDebug = 1;
 
 // Full ray casting //
 function castAllRays() {
+    let alpha = 0, beta = 0, red = 0, green = 0, blue = 0;
+    let xMin = 9999, yMin = 9999;
+    let xMax = -9999, yMax = -9999;
+    if (rayCastDebug) {
+        for (let i = 0; i < screenW; i++) {
+            for (let j = 0; j < screenH; j++) {
+                alpha = (i + 1) / screenW;
+                beta = (j + 1) / screenH;
+                let p = bilinearInterpolate(alpha, beta);
+                let direction = p.subtract(camPosition);
+                if (direction.x < xMin) {
+                    xMin = direction.x;
+                }    
+                if (direction.x > xMax) {
+                    xMax = direction.x;
+                }    
+                if (direction.y < yMin) {
+                    yMin = direction.y;
+                }    
+                if (direction.y > yMax) {
+                    yMax = direction.y;
+                }    
+            }
+        }
+        console.log(xMax, xMin, yMax, yMin);
+    }
+
     for (let i = 0; i < screenW; i++) {
         for (let j = 0; j < screenH; j++) {
-            let alpha = (i + 1) / screenW;
-            let beta = (j + 1) / screenH;
+            alpha = (i + 1) / screenW;
+            beta = (j + 1) / screenH;
             let p = bilinearInterpolate(alpha, beta);
             let direction = p.subtract(camPosition);
             let ray = new Ray(p, direction);
 
             if (rayCastDebug) {
-                let red = alpha * 255;
-                let green = beta * 255;
-                let blue = 40;
-                ctx.fillStyle = "rgba("+red+","+green+","+blue+")";
+                let xScale = Math.floor(((direction.x - xMin) / (xMax - xMin)) * 255 + 1) / 255;
+                let yScale = Math.floor(((direction.y - yMin) / (yMax - yMin)) * 255 + 1) / 255;
+                red = xScale * 255;
+                green = yScale * 255;
+                blue = 50;
+                ctx.fillStyle = "rgb("+red+","+green+","+blue+")";
                 ctx.fillRect(i, j, 1, 1);
             }
         }
@@ -98,9 +133,6 @@ function castAllRays() {
 // bilinear interpolation for ray vector determination //
 function bilinearInterpolate(alpha, beta) {
     let x1 = imPVec1, x2 = imPVec2, x3 = imPVec3, x4 = imPVec4;
-    
-    x3.scale(1-alpha);
-    x4.scale(alpha);
 
     let t = (x1.scale(1-alpha)).add(x2.scale(alpha));
     let b = (x3.scale(1-alpha)).add(x4.scale(alpha));
@@ -108,4 +140,5 @@ function bilinearInterpolate(alpha, beta) {
     return p;
 }
 //////////////////////////////////////////////////////////
+
 ///////////////////////////////////////////////////////////////
